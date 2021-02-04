22b821ad2a12f32a324150213ae1fe471812996b
banerjee999 <78523818+banerjee999@users.noreply.github.com>
2021-02-04 12:48:37 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,11 +1,10 @@
-
-
 import streamlit as st
 import altair as alt
 import pandas as pd
 import numpy as np
 import os, urllib, cv2
 
+# Streamlit encourages well-structured code, like starting execution in a main() function.
 def main():
     # Render the readme as markdown using st.markdown.
     readme_text = st.markdown(get_file_content_as_string("instructions.md"))
@@ -14,7 +13,7 @@ def main():
     for filename in EXTERNAL_DEPENDENCIES.keys():
         download_file(filename)
 
-
+    # Once we have the dependencies, add a selector for the app mode on the sidebar.
     st.sidebar.title("What to do")
     app_mode = st.sidebar.selectbox("Choose the app mode",
         ["Show instructions", "Run the app", "Show the source code"])
@@ -27,6 +26,7 @@ def main():
         readme_text.empty()
         run_the_app()
 
+# This file downloader demonstrates Streamlit animation.
 def download_file(file_path):
     # Don't download the file twice. (If possible, verify the download using the file length.)
     if os.path.exists(file_path):
@@ -35,7 +35,7 @@ def download_file(file_path):
         elif os.path.getsize(file_path) == EXTERNAL_DEPENDENCIES[file_path]["size"]:
             return
 
-
+    # These are handles to two visual elements to animate.
     weights_warning, progress_bar = None, None
     try:
         weights_warning = st.warning("Downloading %s..." % file_path)
@@ -52,25 +52,27 @@ def download_file(file_path):
                     counter += len(data)
                     output_file.write(data)
 
-  
+                    # We perform animation by overwriting the elements.
                     weights_warning.warning("Downloading %s... (%6.2f/%6.2f MB)" %
                         (file_path, counter / MEGABYTES, length / MEGABYTES))
                     progress_bar.progress(min(counter / length, 1.0))
 
-.
+    # Finally, we remove these visual elements by calling .empty().
     finally:
         if weights_warning is not None:
             weights_warning.empty()
         if progress_bar is not None:
             progress_bar.empty()
 
+# This is the main app app itself, which appears when the user selects "Run the app".
 def run_the_app():
-   
+    # To make Streamlit fast, st.cache allows us to reuse computation across runs.
+    # In this common pattern, we download data from an endpoint only once.
     @st.cache
     def load_metadata(url):
         return pd.read_csv(url)
 
-
+    # This function uses some Pandas magic to summarize the metadata Dataframe.
     @st.cache
     def create_summary(metadata):
         one_hot_encoded = pd.get_dummies(metadata[["frame", "label"]], columns=["label"])
@@ -83,9 +85,14 @@ def run_the_app():
         })
         return summary
 
+    # An amazing property of st.cached functions is that you can pipe them into
+    # one another to form a computation DAG (directed acyclic graph). Streamlit
+    # recomputes only whatever subset is required to get the right answer!
     metadata = load_metadata(os.path.join(DATA_URL_ROOT, "labels.csv.gz"))
     summary = create_summary(metadata)
 
+    # Uncomment these lines to peek at these DataFrames.
+    # st.write('## Metadata', metadata[:1000], '## Summary', summary[:1000])
 
     # Draw the UI elements to search for objects (pedestrians, cars, etc.)
     selected_frame_index, selected_frame = frame_selector_ui(summary)
@@ -93,10 +100,10 @@ def run_the_app():
         st.error("No frames fit the criteria. Please select different label or number.")
         return
 
-
+    # Draw the UI element to select parameters for the YOLO object detector.
     confidence_threshold, overlap_threshold = object_detector_ui()
 
-
+    # Load the image from S3.
     image_url = os.path.join(DATA_URL_ROOT, selected_frame)
     image = load_image(image_url)
 
@@ -177,7 +184,8 @@ def get_file_content_as_string(path):
     response = urllib.request.urlopen(url)
     return response.read().decode("utf-8")
 
-
+# This function loads an image from Streamlit public repo on S3. We use st.cache on this
+# function as well, so we can reuse the images across runs.
 @st.cache(show_spinner=False)
 def load_image(url):
     with urllib.request.urlopen(url) as response:
